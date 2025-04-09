0b1573498e9b65a808b0e96e9ea9299830f71cec
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-04-08 23:32:28 -0300

    adicionando funcionalidade nos botões da lista de uploads

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -2,16 +2,20 @@ import * as Progress from "@radix-ui/react-progress"
 import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react";
 import { Button } from "./ui/button.";
 import { motion } from "motion/react"
-import { Upload } from "../store/uploads";
+import { Upload, useUploads } from "../store/uploads";
 import { formatBytes } from "../utils/format-bytes";
 
 interface UploadWidgetUploadItemProps {
 	upload: Upload,
+	uploadId: string
 }
 
-export function UploadWidgetUploadItem({ 
-	upload
+export function UploadWidgetUploadItem({
+	upload,
+	uploadId
 }: UploadWidgetUploadItemProps) {
+	const cancelUpload = useUploads(store => store.cancelUpload)
+
 	return (
 		<motion.div
 			className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
@@ -35,31 +39,53 @@ export function UploadWidgetUploadItem({
 						</span>
 					</span>
 					<div className="size-1 rounded-full bg-zinc-700" />
-					<span>45%</span>
+					{upload.status === "success" && <span>100%</span>}
+					{upload.status === "progress" && <span>45%</span>}
+					{upload.status === "error" && <span className="text-red-400">Error</span>}
+					{upload.status === "canceled" && <span className="text-amber-400">Cancelado</span>}
 				</span>
 			</div>
 
-			<Progress.Root className="bg-zinc-800 rounded-full h-1 overflow-hidden" >
-				<Progress.Indicator className="bg-indigo-50 h-1" style={{ width: '43%' }} />
+			<Progress.Root
+				data-status={upload.status}
+				className="group bg-zinc-800 rounded-full h-1 overflow-hidden"
+			>
+				<Progress.Indicator
+					className="bg-indigo-50 h-1 group-data-[status=success]:bg-green-400 group-data-[status=error]:bg-red-400 group-data-[status=canceled]:bg-yellow-400"
+					style={{ width: upload.status === "progress" ? "43%" : "100%" }}
+				/>
 			</Progress.Root>
 
 			<div className="absolute top-2.5 right-2.5 flex items-center gap-1">
-				<Button size="icon-sm">
+				<Button
+					disabled={upload.status !== "success"}
+					size="icon-sm"
+				>
 					<Download className="size-4" strokeWidth={1.5} />
 					<span className="sr-only">Download compress image</span>
 				</Button>
 
-				<Button size="icon-sm">
+				<Button
+					disabled={upload.status !== "success"}
+					size="icon-sm"
+				>
 					<Link2 className="size-4" strokeWidth={1.5} />
 					<span className="sr-only">Copy remote URL</span>
 				</Button>
 
-				<Button size="icon-sm">
+				<Button
+					disabled={!["canceled", "error"].includes(upload.status)}
+					size="icon-sm"
+				>
 					<RefreshCcw className="size-4" strokeWidth={1.5} />
 					<span className="sr-only">Retry upload</span>
 				</Button>
 
-				<Button size="icon-sm">
+				<Button
+					disabled={upload.status !== "progress"}
+					size="icon-sm"
+					onClick={() => cancelUpload(uploadId)}
+				>
 					<X className="size-4" strokeWidth={1.5} />
 					<span className="sr-only">cancel upload</span>
 				</Button>
--- src/http/upload-file-to-storage.ts
+++ src/http/upload-file-to-storage.ts
@@ -3,8 +3,11 @@ import axios from "axios"
 interface UploadFileToStorageProps {
 	file: File
 }
+interface uploadFileToStorageOpts {
+	signal?: AbortSignal
+}
 
-export async function uploadFileToStorage({ file }: UploadFileToStorageProps) {
+export async function uploadFileToStorage({ file }: UploadFileToStorageProps, opts?: uploadFileToStorageOpts) {
 	const data = new FormData()
 
 	data.append("file", file)
@@ -12,7 +15,8 @@ export async function uploadFileToStorage({ file }: UploadFileToStorageProps) {
 	const response = await axios.post("http://localhost:3333/uploads", data, {
 		headers: {
 			"Content-Type": "multipart/form-data",
-		}
+		},
+		signal: opts?.signal,
 	})
-	return {url: response.data.url}
+	return { url: response.data.url }
 }
